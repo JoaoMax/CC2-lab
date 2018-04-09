577db117201f5fa80799ca6d9589321429f47fa6
JoaoMax <joao_max7@hotmail.com>
2018-04-09 12:32:13 -0500

    Update Ejercicio_8.cpp

--- Ejercicio_8.cpp
+++ Ejercicio_8.cpp
@@ -1,38 +1,54 @@
 #include<iostream>
-#include<conio.h>
 #include<stdlib.h>
 
 using namespace std;
 
-void intercambio(int arr1[], int arr2[], int n)
+void intercambio(char arr1[], char arr2[], int n)
 {
-    for (int i=0; i<n;i++)
-    {
-        arr2[i]=arr1[n-i-1];
-    }
-    for (int i=0;i<n;i++)
-    {
-        cout<<arr1[i]<<" ";
-    }
-    for (int i=0;i<n;i++)
-    {
-        cout<<arr2[i]<<" ";
+    int aux;
+    for(int i=0;i<n;i++){
+        aux=arr1[i];
+        arr1[i]=arr2[i];
+        arr2[i]=aux;
     }
 }
 int main()
 {
-    int n, v;
-    cout<<"Ingrese el tamaño del array 1: ";cin>>n;
-    int arr1[n];
-    int arr2[n];
+    int n;
+    char v;
+    cout<<"Ingrese el tamaÃ±o del array 1: ";cin>>n;
+    char arr1[n];
+    char arr2[n];
     for (int i=0;i<n;i++)
     {
-        cout<<"Ingresa el valor numero "<<i+1<<" del array 1: ";cin>>v;
+        cout<<"Ingresa caracter numero "<<i+1<<" del array 1: ";cin>>v;
         arr1[i]=v;
     }
-    intercambio(arr1, arr2, n);
-
+    for (int i=0;i<n;i++)
+    {
+        cout<<"Ingresa caracter numero "<<i+1<<" del array 2: ";cin>>v;
+        arr2[i]=v;
+    }
+    cout<<"Cadena 1: ";
+    for(int i=0;i<n;i++){
+        cout<<arr1[i];
+    }
+    cout<<endl;
+    cout<<"Cadena 2: ";
+    for(int i=0;i<n;i++){
+        cout<<arr2[i];
+    }
+    cout<<endl;
+   intercambio(arr1, arr2, n);
+    cout<<"Nueva Cadena 1: ";
+    for(int i=0;i<n;i++){
+        cout<<arr1[i];
+    }
+    cout<<endl;
+    cout<<"Nueva Cadena 2: ";
+    for(int i=0;i<n;i++){
+        cout<<arr2[i];
+    }
     return 0;
 }
-
 
